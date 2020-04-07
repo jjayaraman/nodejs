25268267d52934f476eb3ad6b27510231fc09371
jjayaraman <india.jai@gmail.com>
2020-04-07 07:10:05 +0100

    cleanup

---
+++ passport-express/build/app.js
@@ -0,0 +1,37 @@
+"use strict";
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var indexRouter = require("./routes/index");
+var apiRouter = require("./routes/api");
+// const passport = require("passport"),
+//   LocalStrategy = require("passport-local").Strategy;
+var app = express();
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
+app.use("/api", apiRouter);
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(
+//   new LocalStrategy(function(username, password, done) {
+//     console.log("User", User);
+//     User.findOne({ username: username }, function(err, user) {
+//       if (err) {
+//         return done(err);
+//       }
+//       if (!user) {
+//         return done(null, false, { message: "Incorrect username." });
+//       }
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: "Incorrect password." });
+//       }
+//       return done(null, user);
+//     });
+//   })
+// );
+module.exports = app;
---
+++ passport-express/routes/api.ts
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+
+// const passport = require("passport"),
+//   LocalStrategy = require("passport-local").Strategy;
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
+});
+
+// router.post("/login", passport.authenticate("local"), function(req, res) {
+//   // If this function gets called, authentication was successful.
+//   // `req.user` contains the authenticated user.
+//   res.redirect("/users/" + req.user.username);
+// });
+
+module.exports = router;
